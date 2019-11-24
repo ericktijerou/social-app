a27e19077142d25f0811324c44995058c956b684
Erick <ericktijerou@gmail.com>
2019-11-24 15:54:12 -0500

    Fix tests after config offline first

--- app/src/test/java/erick/tijerou/socialapp/data/UserRepositoryTest.kt
+++ app/src/test/java/erick/tijerou/socialapp/data/UserRepositoryTest.kt
@@ -21,6 +21,7 @@ class UserRepositoryTest {
     @Test
     fun `should return a list containing users`() = runBlocking {
         coEvery { dataStore.getUsers() } returns arrayListOf(oneUserModel)
+        coEvery { dataStore.isExpired() } returns false
         val result = userRepository.getUsers(false)
         coVerify { dataStore.getUsers() }
         Assert.assertEquals(1, result.size)
